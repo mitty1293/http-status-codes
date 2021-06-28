b12c4bf04e1c184292a9aa9a52f07c4430a79a47
mitty1293 <k.fmichi@gmai.com>
2021-06-29 06:13:48 +0900

    viewの雛形作成

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,7 +1,12 @@
 from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route('/<int:status_cd>')
-def return_status_code:
+def return_status_code(status_cd):
     pass
 
 # 接続テスト用

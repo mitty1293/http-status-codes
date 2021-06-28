01e8bd415587806b0b36abe65297d78ecb9b2b51
mitty1293 <k.fmichi@gmai.com>
2021-06-29 05:57:47 +0900

    viewの雛形作成

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,10 @@
 from app import app
 
-@app.route('/')
-def hello_world():
-    return 'hello world'
+@app.route('/<int:status_cd>')
+def return_status_code:
+    pass
+
+# 接続テスト用
+@app.route('/test')
+def test():
+    return 'test OK.'

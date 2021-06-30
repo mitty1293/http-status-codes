a8644446c28e58f1edc4d90d3bd378cac114eed5
mitty1293 <k.fmichi@gmai.com>
2021-07-01 00:00:56 +0900

    ステータスコードの拡充

--- flask/app/const.py
+++ flask/app/const.py
@@ -3,9 +3,17 @@ from types import MappingProxyType
 class HTTPStatusCode():
     def __init__(self):
             self.status_line = MappingProxyType({
+                100:"Continue",
                 200:"OK",
+                400:"Bad Request",
+                401:"Unauthorized",
                 403:"Forbidden",
                 404:"Not Found",
-                500:"Internal Server Error"
+                500:"Internal Server Error",
+                501:"Not Implemented",
+                502:"Bad Gateway",
+                503:"Service Unavailable",
+                504:"Gateway Timeout",
+                505:"HTTP Version Not Supported"
             })
 

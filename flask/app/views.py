92904ae2a55fc5d6f646ca3ac449d0d25d48aeed
mitty1293 <k.fmichi@gmai.com>
2021-06-30 13:56:52 +0900

    constを別ファイルにしているとなぜか動かないので一旦views.pyに書き込む

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,20 @@
 from app import app
 from flask import render_template, Response
+#from const import HTTPStatusCode
+# -------------------------------
+from types import MappingProxyType
+
+class HTTPStatusCode():
+    def __init__(self):
+            self.status_line = MappingProxyType({
+                200:"OK",
+                403:"Forbidden",
+                404:"Not Found",
+                500:"Internal Server Error"
+            })
+#--------------------------------
+
+responce = HTTPStatusCode()
 
 @app.route('/')
 def index():
@@ -7,9 +22,12 @@ def index():
 
 @app.route('/<int:status_cd>')
 def return_status_code(status_cd):
-    return Response(status=500)
+    if status_cd in responce.status_line.keys():
+        return Response(status=status_cd)
+    else:
+        return Response(status=404)
 
-# 接続テスト用
+# connection test
 @app.route('/test')
 def test():
     return 'test OK.'

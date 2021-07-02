478c2870dc92bb1eadcaaeb2cdd2e1cf7a233ce0
mitty1293 <k.fmichi@gmai.com>
2021-07-02 23:28:42 +0900

    レスポンスコードの拡充

--- flask/app/const.py
+++ flask/app/const.py
@@ -4,11 +4,16 @@ class HTTPStatusCode():
     def __init__(self):
             self.status_line = MappingProxyType({
                 100:"Continue",
+                102:"Processing",
                 200:"OK",
+                201:"Created",
+                204:"No Content",
+                301:"Moved Permanently",
                 400:"Bad Request",
                 401:"Unauthorized",
                 403:"Forbidden",
                 404:"Not Found",
+                408:"Request Timeout",
                 500:"Internal Server Error",
                 501:"Not Implemented",
                 502:"Bad Gateway",

4aaf0e2e919781403577c688c5ad83d973ac7bec
mitty1293 <k.fmichi@gmail.com>
2022-05-12 00:42:59 +0900

    htmlの表示まで確認済

--- return_http_status_code/application.py
+++ return_http_status_code/application.py
@@ -1,18 +1,23 @@
 from http import HTTPStatus
-from app import app
-from flask import render_template, Response
 
-@app.route('/')
+from flask import Flask, Response, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
     return render_template("index.html", status_lines=HTTPStatus)
 
-@app.route('/<int:status_cd>')
+
+@app.route("/<int:status_cd>")
 def return_status_code(status_cd):
     if status_cd in [e.value for e in HTTPStatus]:
         return Response(status=status_cd)
     return Response(status=404)
 
+
 # connection test
-@app.route('/test')
+@app.route("/test")
 def test():
-    return 'test OK.'
+    return "test OK."

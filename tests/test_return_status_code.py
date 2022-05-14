f7dd21122794a1fa001f942053a8fef041fcc5b2
mitty1293 <k.fmichi@gmail.com>
2022-05-15 01:59:06 +0900

    テストの記述

--- tests/test_return_status_code.py
+++ tests/test_return_status_code.py
@@ -1,6 +1,23 @@
+from http import HTTPStatus
+
 import pytest
 from return_http_status_code.application import app
 
+valid_httpstatus = [e.value for e in HTTPStatus]
+invalid_httpstatus = list(set([i for i in range(200, 600)]) - set(valid_httpstatus))
 
-def test_return_status_code():
-    pass
+
+@pytest.mark.parametrize("status_cd", valid_httpstatus)
+def test_return_status_code_validstatus(status_cd):
+    app.config["TESTING"] = True
+    rv = app.test_client().get(f"/{status_cd}")
+    assert status_cd == rv.status_code
+    assert f"{HTTPStatus(status_cd).phrase}" in rv.data.decode()
+
+
+@pytest.mark.parametrize("status_cd", invalid_httpstatus)
+def test_return_status_code_invalidstatus(status_cd):
+    app.config["TESTING"] = True
+    rv = app.test_client().get(f"/{status_cd}")
+    assert status_cd == rv.status_code
+    assert "Non-standard Code" in rv.data.decode()

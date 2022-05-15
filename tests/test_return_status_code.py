7253aaa9028269d5110cd65d3cb85414f9f467e7
mitty1293 <k.fmichi@gmail.com>
2022-05-15 21:05:07 +0900

    テストを2つにまとめた

--- tests/test_return_status_code.py
+++ tests/test_return_status_code.py
@@ -4,7 +4,7 @@ import pytest
 from return_http_status_code.application import app
 
 valid_httpstatus = [e.value for e in HTTPStatus]
-invalid_httpstatus = list(set([i for i in range(200, 600)]) - set(valid_httpstatus))
+invalid_httpstatus = list(set([i for i in range(100, 600)]) - set(valid_httpstatus))
 
 
 @pytest.mark.parametrize("status_cd", valid_httpstatus)
@@ -12,7 +12,10 @@ def test_return_status_code_valid_httpstatus(status_cd):
     app.config["TESTING"] = True
     rv = app.test_client().get(f"/{status_cd}")
     assert status_cd == rv.status_code
-    assert f"{HTTPStatus(status_cd).phrase}" in rv.data.decode()
+    # Response with body or Response with empty body(100, 101, 102, 103, 204, 304)
+    assert (
+        f"{HTTPStatus(status_cd).phrase}" in rv.data.decode() or "" == rv.data.decode()
+    )
 
 
 @pytest.mark.parametrize("status_cd", invalid_httpstatus)
@@ -20,4 +23,5 @@ def test_return_status_code_invalid_httpstatus(status_cd):
     app.config["TESTING"] = True
     rv = app.test_client().get(f"/{status_cd}")
     assert status_cd == rv.status_code
-    assert "Unknown" in rv.data.decode()
+    # Response with body or Response with empty body(100-199)
+    assert "Unknown" in rv.data.decode() or "UNKNOWN" in rv.status
